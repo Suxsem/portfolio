86e3ea2a9b6af0b85e4e6d1581a043977c236620
Suxsem <Suxsem@users.noreply.github.com>
2017-12-31 12:14:16 +0100

    sw

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,11 +1,10 @@
 if ('serviceWorker' in navigator) {
    navigator.serviceWorker.register('./serviceWorker.js', {
       scope: '.'
-  }).then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-   }, function(err) {
+  }).catch(function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
    });
 }
+
+self.addEventListener('fetch', function(event) { } );
